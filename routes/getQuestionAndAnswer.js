12fd4edf3c3dba08120aa6b2bc249150fbc7f65e
Anmol varma <anmol_varma@github.com>
2018-02-27 19:23:24 +0530

    Modularize the code to reuse it

--- routes/getQuestionAndAnswer.js
+++ routes/getQuestionAndAnswer.js
@@ -1,29 +1,34 @@
 const rp = require('request-promise');
 
+const getAnswers = () => {
+  const getAllQuestionUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allQuestions';
+  const getAnswerByQuestionId = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/';
+  let allQuestions;
+  return rp.get(getAllQuestionUrl)
+    .then((result) => {
+      const list = JSON.parse(result);
+      allQuestions = list.allQuestions;
+      return list.allQuestions.map(eachQuestion => rp.get(`${getAnswerByQuestionId}${eachQuestion.questionId}`));
+    })
+    .then(list => Promise.all(list))
+    .then(answerList => allQuestions.map((entry, index) => {
+      const newEntry = entry;
+      newEntry.answer = JSON.parse(answerList[index]).answer;
+      return newEntry;
+    }))
+    .then(result => ({ allQuestionsWithAnswer: result }))
+    .catch((error) => {
+      console.log(error.message);
+      return { statusCode: 500, message: 'Error in external api' };
+    });
+};
+
+
 module.exports = [
   {
     method: 'GET',
     path: '/getQuestionsAndAnswers',
     handler: (request, reply) => {
-      const getAllQuestionUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allQuestions';
-      const getAnswerByQuestionId = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/';
-      let allQuestions;
-      rp.get(getAllQuestionUrl)
-        .then((result) => {
-          const list = JSON.parse(result);
-          allQuestions = list.allQuestions;
-          return list.allQuestions.map(eachQuestion => rp.get(`${getAnswerByQuestionId}${eachQuestion.questionId}`));
-        })
-        .then(list => Promise.all(list))
-        .then(answerList => allQuestions.map((entry, index) => {
-          const newEntry = entry;
-          newEntry.answer = JSON.parse(answerList[index]).answer;
-          return newEntry;
-        }))
-        .then(result => reply({ allQuestionsWithAnswer: result }))
-        .catch((error) => {
-          console.log(error.message);
-          return reply({ statusCode: 500, message: 'Error in external api' });
-        });
+      getAnswers().then(result => reply(result));
     },
   }];
