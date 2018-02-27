04bf66c2a0e80f5a992144db5809a2b86ceda27e
Anmol varma <anmol_varma@github.com>
2018-02-27 22:14:33 +0530

    Helper function not required as column stores JSON

--- helper/getArrayFromString.js
+++
@@ -1,6 +0,0 @@
-const getBackOptions = (stringArrayWithOptions) => {
-  const optionsArray = stringArrayWithOptions.split(';:*:;');
-  return optionsArray.slice(0, optionsArray.length - 1);
-};
-
-module.exports = getBackOptions;
--- tests/helper/getArrayFromString.test.js
+++
@@ -1,9 +0,0 @@
-const testFunction = require('./getArrayFromString.test');
-
-describe('testing whether the function returns an array for string', () => {
-  test('the function should return a array', (done) => {
-    const input = 'option1;:*:;option2;:*:;option3';
-    expect(testFunction(input)[0]).toBe('option1');
-    done();
-  });
-});
