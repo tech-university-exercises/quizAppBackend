02d3c187200e183b0fd5b3d762cfe585861bf0f3
Anmol varma <anmol_varma@github.com>
2018-02-27 16:22:18 +0530

    Hapi server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const Hapi = require('hapi');
+const Routes = require('./routes');
+
+const server = new Hapi.Server();
+server.connection({
+  port: 8080,
+  host: 'localhost',
+});
+server.route(Routes);
+
+if (!module.parent) {
+  server.start((err) => {
+    if (err) {
+      throw (err);
+    }
+    console.log(`Server running at ${server.uri.info}`);
+  });
+}
+
+module.exports = server;
